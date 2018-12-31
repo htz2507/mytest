cdf22b2a0ad8ab5457de1809a6a1775606c29b53
htz2507 <43902207+htz2507@users.noreply.github.com>
2018-12-31 09:15:35 +0800

    Update App.java

--- src/main/java/com/test/mytest/mytest/App.java
+++ src/main/java/com/test/mytest/mytest/App.java
@@ -4,14 +4,16 @@ package com.test.mytest.mytest;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("Now It's a git repository!!!");
+        show_name();
     }
-    
+
+
     public void show_name() {
     	System.out.println("My name is Htz.");
     }
